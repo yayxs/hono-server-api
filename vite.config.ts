590b3b0e4709a6045340b706307376c23a272a07
yayxs <leaderywl@163.com>
2024-08-28 15:50:38 +0800

    feat: tsconfig jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-
 import honox from 'honox/vite'
-
+import mdx from '@mdx-js/rollup'
 export default defineConfig({
-  plugins: [honox({})]
+  plugins: [
+    honox({}),
+    mdx({
+      jsxImportSource: 'hono/jsx'
+    })
+  ]
 })
